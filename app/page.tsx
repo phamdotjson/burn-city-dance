67f7f4209e4a0356ed2026fa29518a61da2b50df
Jason Pham <phamjason0@gmail.com>
2024-04-23 19:48:54 +1000

    Move calendar page to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,70 @@
-import Image from "next/image";
+import EventCard, { EventCardProps } from "@/components/EventCard";
+import SearchBar from "@/components/SearchBar";
+import { EventTagsEnum, EventTypeEnum } from "@/constants/EventEnums";
+import dayjs from "dayjs";
 
-export default function Home() {
+const events: EventCardProps[] = [
+  {
+    eventName: "Waacking Inferno: Vol. 3 - Day 1",
+    organiser: "Burncity Waack",
+    location: "Dancehouse",
+    dateTimeUtc: "2024-12-14T07:00:00.000Z",
+    eventTypes: [EventTypeEnum.Battle],
+    tags: [EventTagsEnum.Waacking],
+    pricingInfo: 'Spectators: $20 | Battlers: $30',
+    image: 'waack.jpg',
+    ticketLink: "humanitix.com",
+    organiserContact: ""
+  },
+  {
+    eventName: "Popping Nation: Vol. 2 - Round 1 Battles",
+    organiser: "Jimmy Neutron Zhu",
+    location: "O2 Dance Studio",
+    dateTimeUtc: "2023-11-12T07:00:00.000Z",
+    eventTypes: [EventTypeEnum.Battle],
+    tags: [EventTagsEnum.Popping],
+    pricingInfo: '$10 Early Bird | $15 @ O2',
+    image: 'pn.jpg',
+    organiserContact: ""
+  },
+  {
+    eventName: "Ocean 2 Sky: Choreography Competition",
+    organiser: "O2 Dance Studio",
+    location: "O2 Dance Studio",
+    dateTimeUtc: "2024-06-28T08:00:00.000Z",
+    eventTypes: [EventTypeEnum.Choreography_Competition, EventTypeEnum.Workshop, EventTypeEnum.Battle],
+
+    image: 'o2s.jpg',
+    pricingInfo: "Spectator tickets from $25",
+    organiserContact: ""
+  },
+  {
+    eventName: "Boogaloo Workshop - Ahrin Tapat WOW!",
+    organiser: "Ahrin Tapat",
+    location: "O2 Dance Studio",
+    dateTimeUtc: "2024-05-14T07:00:00.000Z",
+    eventTypes: [EventTypeEnum.Workshop],
+    tags: [EventTagsEnum.Popping],
+    image: 'tapat.jpg',
+    pricingInfo: "Just buy me KFC",
+    ticketLink: "humanitix.com",
+    organiserContact: ""
+  },
+]
+
+const page = () => {
+  events.sort((x, y) => dayjs(x.dateTimeUtc).unix() - dayjs(y.dateTimeUtc).unix())
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="container">
+      <div className="flex items-center justify-center p-4">
+        <SearchBar />
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {events.map((e, index) => <EventCard key={index} {...e} />)}
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
+
+export default page;
