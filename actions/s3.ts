f174b505680a012d71974a9671a712d043dc00c6
Jason Pham <phamjason0@gmail.com>
2024-04-25 20:22:22 +1000

    Use cloudfront cdn

--- actions/s3.ts
+++ actions/s3.ts
@@ -1,27 +1,9 @@
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-
 const bucketName = process.env.BUCKET_NAME ?? "";
 const bucketRegion = process.env.BUCKET_REGION ?? "";
 const accessKeyId = process.env.ACCESS_KEY_ID ?? "";
 const secretAccessKey = process.env.SECRET_ACCESS_KEY ?? "";
-
-const s3 = new S3Client({
-  credentials: {
-    accessKeyId: accessKeyId,
-    secretAccessKey: secretAccessKey
-  },
-  region: bucketRegion
-})
+const distributionName = process.env.CLOUDFRONT_DISTRIBUTION_DOMAIN_NAME ?? "";
 
 export const getImageUrlFromS3Bucket = async (imageName: string) => {
-  const params = {
-    Bucket: bucketName,
-    Key: imageName
-  }
-
-  const command = new GetObjectCommand(params);
-  const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
-
-  return url;
+  return `https://${distributionName}/${imageName}`;
 }
